3ab0cd6d80be61aa231e2fd8ccef76a85f217b52
dl <dlott2@Gmail.com>
2016-03-14 22:24:52 -0400

    temp

---
+++ webStuff/javascript/scripts/koMitDatePicker.js
@@ -0,0 +1,50 @@
+﻿ko.bindingHandlers.mitdatepicker = {
+    init: function (element, valueAccessor, allBindingsAccessor) {
+        // show a picker image after the input (action not implemented yet)
+        var observable = valueAccessor();
+        var showPickerImage = allBindingsAccessor.get('showPickerImage') || false;
+        if (showPickerImage) {
+            $(element).after("<i class='icon-date datepicker-image' style='cursor: pointer'></i>");
+            $(".datepicker-image").click(function () {
+                alert("action not implemented");
+            });
+        }
+        // handler for when the user changes the value in the input
+        ko.utils.registerEventHandler(element, 'change', function () {
+            var v;
+            var value = ko.utils.unwrapObservable(observable);
+            if (typeof value === "object") {
+                var format = allBindingsAccessor.get('format') || "MM/DD/YYYY";
+                v = value.ismit ? value : mit(value);
+                var mod = v.minuteOfDay();
+                v = mit($(element).val(), format).add(mod, "minutes");
+            } else {
+                // todo - support more formats?
+                // todo - use bootstrap datepicker
+                throw "don't know how to interpet '" + value + "'";
+            }
+            if (observable() === v) {
+                observable(v);
+                ko.bindingHandlers.mitdatepicker.update(element, valueAccessor, allBindingsAccessor);
+            } else {
+                observable(v);
+            }
+        });
+        // select the entire input text when it gets focus
+        $(element).click(function () {
+            $(this).select();
+        });
+    },
+    // when the observable is updated...
+    update: function (element, valueAccessor, allBindingsAccessor) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        var format = allBindingsAccessor.get('format') || "MM/DD/YYYY";
+        if (typeof value === "object") {
+            $(element).val(value.format(format));
+        } else {
+            // todo - support more formats?
+            // todo - use bootstrap datepicker
+            throw "don't know how to interpet '" + value + "'";
+        }
+    }
+};
---
+++ webStuff/javascript/scripts/koMitTimePicker.js
@@ -0,0 +1,49 @@
+﻿ko.bindingHandlers.mittimepicker = {
+    init: function (element, valueAccessor, allBindingsAccessor) {
+        // show a picker image after the input (action not implemented yet)
+        var observable = valueAccessor();
+        var showPickerImage = allBindingsAccessor.get('showPickerImage') || false;
+        if (showPickerImage) {
+            $(element).after("<i class='icon-time timepicker-image' style='cursor: pointer'></i>");
+            $(".timepicker-image").click(function () {
+                alert("action not implemented");
+            });
+        }
+        // handler for when the user changes the value in the input
+        // register change event
+        ko.utils.registerEventHandler(element, 'change', function () {
+            var v;
+            var value = ko.utils.unwrapObservable(observable);
+            if (typeof value === "object") {
+                v = value.ismit ? value : mit(value);
+                v = v.setTime($(element).val());
+            } else {
+                // assuming tod in minutes
+                var m = mit(0).startOf("day").setTime($(element).val());
+                v = m.diff(mit(0).startOf("day"), "minutes");
+            }
+            if (observable() === v) {
+                observable(v);
+                ko.bindingHandlers.mittimepicker.update(element, valueAccessor, allBindingsAccessor);
+            } else {
+                observable(v);
+            }
+        });
+        // select the entire input text when it gets focus
+        $(element).click(function () {
+            $(this).select();
+        });
+    },
+    // when the observable is updated...
+    update: function (element, valueAccessor, allBindingsAccessor) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        var format = allBindingsAccessor.get('format') || "hh:mm A";
+        if (typeof value === "object") {
+            $(element).val(value.format(format));
+        } else {
+            // assuming tod in minutes
+            var m = mit(0).startOf("day").add(value, "minutes");
+            $(element).val(m.format(format));
+        }
+    }
+};
